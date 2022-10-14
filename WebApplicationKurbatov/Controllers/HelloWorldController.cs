de63f0d93fcc09010467eaf97453d596311c113e
315 <315@315-16>
2022-10-14 12:06:34 +0300

    Добавьте файлы проекта.

---
+++ WebApplicationKurbatov/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationKurbatov.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my defaul action...";
+        }
+
+        public string Welcome()
+        {
+            return "This is the welcome action method...";
+        }
+    }
+}
